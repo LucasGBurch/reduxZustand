e0b9f5e3a70320a03766764629939e242ae488ed
burchlg <88804181+LucasGBurch@users.noreply.github.com>
2024-07-17 09:08:09 -0300

    Criação de interface de loading

--- react-redux/src/store/slices/player.ts
+++ react-redux/src/store/slices/player.ts
@@ -19,12 +19,14 @@ export interface PlayerState {
   course: Course | null
   currentModuleIndex: number
   currentLessonIndex: number
+  isLoading: boolean
 }
 
 const initialState: PlayerState = {
   course: null,
   currentModuleIndex: 0,
   currentLessonIndex: 0,
+  isLoading: true,
 }
 
 export const loadCourse = createAsyncThunk(
@@ -70,8 +72,12 @@ export const playerSlice = createSlice({
     },
   },
   extraReducers(builder) {
+    builder.addCase(loadCourse.fulfilled, (state) => {
+      state.isLoading = true
+    }),
     builder.addCase(loadCourse.fulfilled, (state, action) => {
       state.course = action.payload
+      state.isLoading = false
     })
   },
 })
